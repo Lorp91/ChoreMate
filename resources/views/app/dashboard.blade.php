448bd6d7971b5d0b61da402d2bde3848cc0c44cb
Lorp91 <92474455+Lorp91@users.noreply.github.com>
2026-02-06 15:27:17 +0100

    dashboard placeholder changed

--- resources/views/app/dashboard.blade.php
+++ resources/views/app/dashboard.blade.php
@@ -1,9 +1,5 @@
 <x-layout.app title="Dashboard">
-    <div class="h-screen flex flex-col justify-center items-center gap-5">
+    <div class="mt-15 flex flex-col justify-center items-center gap-5">
         <h2 class="text-2xl">ChoreMate - Dashboard</h2>
-
-{{--        <h3 class="text-xl text-center">{{ session('current_household_id') ?? 'Keine Household id' }}</h3>--}}
-
-{{--        <x-household.selector :households="$households"/>--}}
     </div>
 </x-layout.app>
