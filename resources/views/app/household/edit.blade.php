e1638c160866b8e17eea391b7484566042d1e12b
Lorp91 <92474455+Lorp91@users.noreply.github.com>
2026-02-10 18:52:57 +0100

    household edit styling refined

--- resources/views/app/household/edit.blade.php
+++ resources/views/app/household/edit.blade.php
@@ -1,21 +1,29 @@
 <x-layout.app title="{{ $household->name }} bearbeiten">
-    <h2 class="text-3xl">{{ $household->name }}</h2>
-    <form method="POST" action="{{ route('households.update', $household) }}">
-        @csrf
-        @method('PUT')
+    <div class="p-10">
+        <form
+            method="POST"
+            action="{{ route('households.update', $household) }}"
+            class="max-w-xl"
+        >
+            @csrf
+            @method('PUT')
 
-        <x-form.field
-            name="name"
-            label="Name"
-            value="{{ old('name', $household->name) }}"
-        />
+            <x-form.field
+                name="name"
+                label="Name"
+                value="{{ old('name', $household->name) }}"
+            />
 
-        <button type="submit" form="delete-household" class="btn btn-warning">Loeschen</button>
-        <button type="submit" class="btn btn-primary">Update</button>
-    </form>
+            <div class="mt-5 flex flex-row-reverse items-center gap-3">
+                <button type="submit" class="btn btn-primary">Speichern</button>
+                <button type="submit" form="delete-household" class="btn btn-warning">Löschen</button>
+                <a href="{{ route('households.index') }}" class="btn btn-ghost">abbrechen</a>
+            </div>
+        </form>
 
-    <form id="delete-household" method="POST" action="{{ route('households.destroy', $household) }}">
-        @csrf
-        @method('DELETE')
-    </form>
+        <form id="delete-household" method="POST" action="{{ route('households.destroy', $household) }}">
+            @csrf
+            @method('DELETE')
+        </form>
+    </div>
 </x-layout.app>
