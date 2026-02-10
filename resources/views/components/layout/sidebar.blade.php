7898069a0973bab373af342aaeddb5577926cba9
Lorp91 <92474455+Lorp91@users.noreply.github.com>
2026-02-10 11:58:14 +0100

    sidebar styling refined

--- resources/views/components/layout/sidebar.blade.php
+++ resources/views/components/layout/sidebar.blade.php
@@ -1,26 +1,53 @@
 @props(['currentHousehold', 'currentRooms'])
 
-<ul class="menu bg-base-200 min-h-full w-sm p-4">
-    <div class="flex items-center gap-4">
-        <span class="text-4xl">#</span>
-        <h1 class="text-4xl font-semibold">ChoreMate</h1>
+<div class="menu bg-base-200 min-h-full w-sm p-4">
+    <div class="flex items-center gap-2">
+        <x-icon.logo class="w-12"/>
+        <h1 class="text-4xl font-extrabold">ChoreMate</h1>
     </div>
-    <div class="mt-5">
-        <a href="{{ route('households.index') }}" class="text-3xl">{{ $currentHousehold->name }}</a>
+    <div class="mt-7">
+        <a
+            href="{{ route('households.index') }}"
+            class="text-3xl font-bold"
+        >{{ $currentHousehold->name }}</a>
+    </div>
+    <div class="mt-5 flex flex-col gap-2">
+        <a
+            href="{{ route('households.dashboard', $currentHousehold) }}"
+            class="text-xl"
+        >Dashboard</a>
+        <a
+            href=""
+            class="text-xl text-base-content/40 cursor-not-allowed"
+        >Kalender</a>
+        <a
+            href=""
+            class="text-xl text-base-content/40 cursor-not-allowed"
+        >Statistik</a>
+        <a
+            href=""
+            class="text-xl text-base-content/40 cursor-not-allowed"
+        >Alle Aufgaben</a>
     </div>
     <div class="mt-5">
         <div>
-            <h3 class="text-xl">Raeume</h3>
-
-            <ul class="mt-5 space-y-4">
-                <li><a href="{{ route('households.rooms.index', $currentHousehold) }} ">alle Raeume</a></li>
+            <div class="flex justify-between items-center">
+                <h3 class="text-2xl font-semibold">Räume</h3>
+                <a
+                    href="{{ route('households.rooms.create', $currentHousehold) }}"
+                    class="text-sm text-success-content"
+                >+ neuer Raum</a>
+            </div>
+            <ul class="ml-4 mt-1 space-y-2">
                 @foreach($currentRooms as $room)
-                    <li><a href="{{ route('households.rooms.show', [$currentHousehold, $room]) }}">{{ $room->name }}</a>
+                    <li>
+                        <a
+                            href="{{ route('households.rooms.show', [$currentHousehold, $room]) }}"
+                            class="{{ request()->route('room')?->id === $room->id ? 'menu-active' : '' }}"
+                        >{{ $room->name }}</a>
                     </li>
                 @endforeach
-                <li><a href="{{ route('households.rooms.create', $currentHousehold) }}" class="text-accent-content">+
-                        neuer Raum</a></li>
             </ul>
         </div>
     </div>
-</ul>
+</div>
