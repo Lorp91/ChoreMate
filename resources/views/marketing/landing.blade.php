e39dc12297d62f5bed66985e194af1ca9176cdea
Lorp91 <92474455+Lorp91@users.noreply.github.com>
2026-02-10 17:50:14 +0100

    link to app for authed users added

--- resources/views/marketing/landing.blade.php
+++ resources/views/marketing/landing.blade.php
@@ -3,12 +3,15 @@
         <h1 class="text-3xl">ChoreMate - Landingpage</h1>
         <div class="mt-10">
             @auth
-                <form method="POST" action="{{ route('login.destroy') }}">
-                    @csrf
-                    @method('DELETE')
+                <div class="flex items-center gap-4">
+                    <a href="{{ route('households.index') }}" class="btn btn-ghost">zur App</a>
+                    <form method="POST" action="{{ route('login.destroy') }}">
+                        @csrf
+                        @method('DELETE')
 
-                    <button class="btn btn-primary">Logout</button>
-                </form>
+                        <button class="btn btn-primary">Logout</button>
+                    </form>
+                </div>
             @else
                 <a href="{{ route('login.index') }}" class="btn btn-ghost">Login</a>
                 <a href="{{ route('register.index') }}" class="btn btn-primary">Registrieren</a>
