15b730eb57bbe15ceab82591c31434b20c2be4da
Lorp91 <92474455+Lorp91@users.noreply.github.com>
2026-02-10 18:45:12 +0100

    household create styling refined

--- resources/views/app/household/create.blade.php
+++ resources/views/app/household/create.blade.php
@@ -1,14 +1,23 @@
 <x-layout.app title="Haushalt erstellen">
-    <h2 class="text-3xl">Neuer Haushalt</h2>
-    <form method="POST" action="{{ route('households.store') }}">
-        @csrf
+    <div class="p-10">
+        <form
+            method="POST"
+            action="{{ route('households.store') }}"
+            class="max-w-xl"
+        >
+            @csrf
 
-        <x-form.field
-            name="name"
-            label="Name"
-            value="{{ old('name') }}"
-        />
+            <x-form.field
+                name="name"
+                label="Name"
+                placeholder="z.B. Ferienhaus"
+                value="{{ old('name') }}"
+            />
 
-        <button type="submit" class="btn btn-primary">Erstellen</button>
-    </form>
+            <div class="mt-5 flex flex-row-reverse items-center gap-3">
+                <button type="submit" class="btn btn-primary">Erstellen</button>
+                <a href="{{ route('households.index') }}" class="btn btn-ghost">abbrechen</a>
+            </div>
+        </form>
+    </div>
 </x-layout.app>
