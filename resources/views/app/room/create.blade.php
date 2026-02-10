2071c2f2739cad02a5ed415de2410622e4056a27
Lorp91 <92474455+Lorp91@users.noreply.github.com>
2026-02-10 19:10:40 +0100

    room create styling refined

--- resources/views/app/room/create.blade.php
+++ resources/views/app/room/create.blade.php
@@ -1,14 +1,23 @@
 <x-layout.app title="Raum erstellen">
-    <h2 class="text-3xl">Neuer Raum</h2>
-    <form method="POST" action="{{ route('households.rooms.store', [$household]) }}">
-        @csrf
+    <div class="p-10">
+        <form
+            method="POST"
+            action="{{ route('households.rooms.store', [$household]) }}"
+            class="max-w-xl"
+        >
+            @csrf
 
-        <x-form.field
-            name="name"
-            label="Name"
-            value="{{ old('name') }}"
-        />
+            <x-form.field
+                name="name"
+                label="Name"
+                placeholder="z.B. Wohnzimmer"
+                value="{{ old('name') }}"
+            />
 
-        <button type="submit" class="btn btn-primary">Erstellen</button>
-    </form>
+            <div class="mt-5 flex flex-row-reverse items-center gap-3">
+                <button type="submit" class="btn btn-primary">Erstellen</button>
+                <a href="{{ route('households.dashboard', $household) }}" class="btn btn-ghost">abbrechen</a>
+            </div>
+        </form>
+    </div>
 </x-layout.app>
