047ba93a6423deab40e02b46fd376e4c8c8196f9
Lorp91 <92474455+Lorp91@users.noreply.github.com>
2026-02-10 09:45:54 +0100

    roomSelector moved back to sidebar

--- resources/views/components/layout/sidebar.blade.php
+++ resources/views/components/layout/sidebar.blade.php
@@ -1,3 +1,5 @@
+@props(['currentHousehold', 'currentRooms'])
+
 <ul class="menu bg-base-200 min-h-full w-sm p-4">
     <div class="flex items-center gap-4">
         <span class="text-4xl">#</span>
@@ -7,6 +9,18 @@
         <a href="{{ route('households.index') }}" class="text-3xl">{{ $currentHousehold->name }}</a>
     </div>
     <div class="mt-5">
-        <x-room.selector :current-household="$currentHousehold" :current-rooms="$currentRooms"/>
+        <div>
+            <h3 class="text-xl">Raeume</h3>
+
+            <ul class="mt-5 space-y-4">
+                <li><a href="{{ route('households.rooms.index', $currentHousehold) }} ">alle Raeume</a></li>
+                @foreach($currentRooms as $room)
+                    <li><a href="{{ route('households.rooms.show', [$currentHousehold, $room]) }}">{{ $room->name }}</a>
+                    </li>
+                @endforeach
+                <li><a href="{{ route('households.rooms.create', $currentHousehold) }}" class="text-accent-content">+
+                        neuer Raum</a></li>
+            </ul>
+        </div>
     </div>
 </ul>
